0bd0394e87bb17697f8d5843c5c71bd6f7b79c22
Caleb <cmk265@cornell.edu>
2023-02-04 18:14:20 -0500

    nicer graphs

--- scripts/graph-gen.py
+++ scripts/graph-gen.py
@@ -5,40 +5,52 @@ import sys
 import json 
 import matplotlib.pyplot as plt
 import numpy as np 
+import seaborn as sns
+import pandas as pd 
+
+sns.set_theme()
 
 supported_designs = ["alex", "google", "lenet", "mobile", "squeeze", "vgg"]
-sharing_bounds = ["1,1", "4,4", "-1,-1"]
-sharing_settings = ["no-infer-share", "default", "fully-inline"]
+share_bounds = ["1,1", "4,4", "-1,-1"]
+share_settings = ["no-infer-share", "default", "fully-inline"]
 
 if __name__ == "__main__":
     assert (len(sys.argv) == 3), "please provide a design name and a resource name"
     assert sys.argv[1] in supported_designs, f"""design must be one of {supported_designs}"""
     design = sys.argv[1] 
     resource_name = sys.argv[2]
-    file_path = os.path.join("results", design, f"""resource_numbers_{design}.json""") 
-    json_data = json.load(open(file_path))
-    graph_data = []
-    for bound in sharing_bounds:
-      bound_data = []
-      for setting in sharing_settings:
-        bound_data.append(json_data[setting][bound][resource_name])
-      graph_data.append(bound_data)
-      
-    x = np.arange(len(sharing_settings))
-    bound_one = graph_data[0]
-    bound_four = graph_data[1]
-    bound_none = graph_data[2]
-    width = 0.2
-  
-    # plot data in grouped manner of bar type
-    plt.bar(x-0.2, bound_one, width, color='cyan')
-    plt.bar(x, bound_four, width, color='orange')
-    plt.bar(x+0.2, bound_none, width, color='green')
-    plt.xticks(x, sharing_settings)
-    plt.xlabel("Setting")
-    plt.ylabel("Resource Usage")
-    plt.legend(["Bound 1", "Bound 4", "No Bound"])
+    df = pd.read_csv("tables/full-table.csv")
+    data = []
+    design_setting = df.loc[:, 'setting']
+    bound_1 = df.loc[:, f"""{resource_name}_1,1"""]
+    bound_4 = df.loc[:, f"""{resource_name}_4,4"""]
+    bound_none = df.loc[:, f"""{resource_name}_-1,-1"""]
+    for (idx, design_setting) in enumerate(design_setting):
+      for share_setting in share_settings:
+        if design_setting == f"""{design}_{share_setting}""":
+          data.append([share_setting, "1", bound_1[idx]])
+          data.append([share_setting, "4", bound_4[idx]])
+          data.append([share_setting, "None", bound_none[idx]])
+          
+    df = pd.DataFrame(data, columns=['share_setting', 'share_bound', 'usage'])
+    # Set the figure size
+    fig = plt.figure(figsize=(8, 8))
+    fig.add_axes([0.1, 0.1, 0.65, 0.85])
+
+    # grouped barplot
+    ax = sns.barplot(x="share_setting", y="usage", hue="share_bound", order= share_settings ,data=df, errorbar=None)
+    
+    ax.set(title=f"""{resource_name} usage on {design}net""")
+    
+    sns.move_legend(ax, "upper right", bbox_to_anchor=(1.3, 1))
+    
+    fig.savefig('samplefigure', bbox_inches='tight')
+    
     plt.show()
+          
+           
+        
+      
 
     
    
