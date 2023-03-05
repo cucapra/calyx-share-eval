ce536da29ec3794d23c07d6c86dede7356dc7967
Caleb <cmk265@cornell.edu>
2023-03-04 21:14:36 -0500

    added graphs

--- scripts/graph-gen.py
+++ scripts/graph-gen.py
@@ -1,5 +1,5 @@
 '''
-Command line argument: python3 path/to/graph-gen.py <design-name> <resource-name>
+Command line argument: python3 path/to/graph-gen.py <resource-name>
 
 See tables/full-table.csv to see the resources/design names supported 
 '''
@@ -40,44 +40,47 @@ def format(s, space_replacement):
   '''
   replace space_replacement with space, then format each word according to format_word()
   '''
-  s_list = s.split("-")
+  s_list = s.split(space_replacement)
   final_list = [format_word(s) for s in s_list]
   return " ".join(final_list)
+
+def add_data(data, row, design):
+  data.append([design, "No Sharing", row[1]])
+  data.append([design, "Calyx 2020", row[2]])
+  data.append([design, "Share All Primitives", row[3]])
+  data.append([design, "Share All Components", row[4]])
+  data.append([design, "Fully Inline", row[5]])
   
 if __name__ == "__main__":
-    assert (len(sys.argv) == 3), "please provide a design name and a resource name"
-    assert sys.argv[1] in supported_designs, f"""design must be one of {supported_designs}"""
-    design = sys.argv[1] 
-    resource_name = sys.argv[2]
-    df = pd.read_csv("tables/full-table.csv")
+    assert (len(sys.argv) == 2), "please provide a resource name"
+    resource_name = sys.argv[1]
+    df = pd.read_csv("tables/all-stats-table.csv")
     data = []
-    design_setting = df.loc[:, 'design/compiler setting']
-    never_share = df.loc[:, f"""{resource_name}_1,1,1"""]
-    calyx_2020 = df.loc[:, f"""{resource_name}_-1,-1,1"""]
-    always_share = df.loc[:, f"""{resource_name}_-1,-1,-1"""]
-    for (idx, design_setting) in enumerate(design_setting):
-      for share_setting in share_settings:
-        if design_setting == f"""{design}_{share_setting}""":
-          data.append([format(share_setting, "-"), "No Sharing", never_share[idx]])
-          data.append([format(share_setting, "-"), "Calyx 2020 ", calyx_2020[idx]])
-          data.append([format(share_setting, "-"), "Always Share", always_share[idx]])
+    for _,row in df.iterrows():
+      design = format_design_name(row[0].split("_")[0])
+      resource = "_".join(row[0].split("_")[1:])
+      if resource == resource_name:
+        add_data(data, row, design)
+      
           
-    df = pd.DataFrame(data, columns=['Compiler Setting', 'Sharing Type', 'Usage'])
+    df = pd.DataFrame(data, columns=['Design', 'Share Setting', 'Usage'])
     # Set the figure size
     fig = plt.figure(figsize=(8, 8))
     fig.add_axes([0.1, 0.1, 0.65, 0.85])
     
-    formatted_share_setting = [format(s, "-") for s in share_settings]
+    #order = ["No Sharing", "Share Registers and Combinational", "Share All Primitives", "Share All Components", "Fully Inline and Share"]
 
     # grouped barplot
-    ax = sns.barplot(x="Compiler Setting", y="Usage", hue="Sharing Type", order = formatted_share_setting, data=df, errorbar=None)
+    ax = sns.barplot(x="Design", y="Usage", hue="Share Setting", data=df, errorbar=None)
     
-    ax.set(title=f"""{format(resource_name, "_")} Usage on {format_design_name(design)}""")
+    ax.set(title=f"""{format(resource_name, "_")} Usage""")
     
     sns.move_legend(ax, "upper right", bbox_to_anchor=(1.3, 1))
     
+    if not os.path.exists("graphs"):
+      os.makedirs("graphs") 
     # can save figure if we want 
-    # fig.savefig('resource_graph', bbox_inches='tight')
+    fig.savefig(f"""graphs/{format(resource_name, "_")}_usage""", bbox_inches='tight')
     
     plt.show()
           
